c98a66cc034fa4b806945ddd11bc32a67498a11e
skrrrt-and-boom <domin6051@gmail.com>
2022-05-15 20:44:34 +0200

    2 commit

--- script.js
+++ script.js
@@ -9,17 +9,58 @@ mainLogo[0].addEventListener("click", () => {
   for (let i = 0; i < span.length; i++) {
     span[i].classList.remove("main-span");
   }
-  const addAktualności = document.createElement("div");
-  addAktualności.classList.add("Aktualności");
-  //  function with aktualnościami;
-  if (control == 1) {
-    body.appendChild(addAktualności);
+  //  function with aktualności;
+  if (control === 1) {
+    addAktualności();
+    appendFooter();
     control += 1;
   }
 });
+
+//  add Aktualności
+function addAktualności() {
+  let addAktualności = document.querySelectorAll(".contener_top");
+  for ( i of addAktualności) {
+    i.removeAttribute("hidden");
+  }
+}
+
+let showHide = 1;
+
+//  show/hide content when click
+function showAktualności() {
+  const containerTop = document.getElementsByClassName("contener_top");
+  const containerContent = document.getElementsByClassName("contener_tresc");
+  for (let i = 0; i < containerTop.length; i++) {
+    containerTop[i].addEventListener("click", () => {
+      if (control > 1 && showHide % 2 != 0) {
+        containerContent[i].removeAttribute("hidden");
+        showHide += 1;
+      } else if (control > 1 && showHide % 2 == 0) {
+          containerContent[i].setAttribute("hidden", false);
+          showHide += 1;
+      }
+    });
+  }
+}
+
+showAktualności();
+
+//  add footer
+function appendFooter() {
+  const addFooter = document.createElement("div");
+  addFooter.classList.add("Footer");
+  return body.appendChild(addFooter);
+}
 
 //  delate scrollbar
 const styleElement = document.createElement("style");
 styleElement.id = "remove-scroll";
 styleElement.textContent = "html::-webkit-scrollbar{display:none !important}" + "body::-webkit-scrollbar{display:none !important}";
 body.appendChild(styleElement);
+
+//  awsome style
+const face = document.querySelector("a");
+face.addEventListener("click", () => {
+  face.textContent = "@_@";
+});
